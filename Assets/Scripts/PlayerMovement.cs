54fb5f49558ffbd90b4ea87a75e0bf288da8ae94
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-04-30 15:40:58 +0200

    Migrated ShaderProject into 2.5D

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,22 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Sprites")]
-    [SerializeField] private SpriteRenderer bodySpriteRenderer;
-    [SerializeField] private SpriteRenderer rightLegSpriteRenderer;
-    [SerializeField] private SpriteRenderer leftLegSpriteRenderer;
-    
     [Header("Parameters")]
     [SerializeField] private float speed;
-    
+
+    private SpriteRenderer _spriteRenderer;
     private Rigidbody _rigidbody;
-    
     private Animator _animator;
+    
     private float _xInput;
     private float _zInput;
     
     private void Awake()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
     }
@@ -28,17 +25,29 @@ public class PlayerMovement : MonoBehaviour
         _zInput = Input.GetAxis("Vertical");
         if (_xInput != 0 || _zInput != 0)
         {
-            _animator.SetBool("isRunning",true);
+            _animator.SetBool("Run",true);
         }
         else
         {
-            _animator.SetBool("isRunning",false);
+            _animator.SetBool("Run",false);
         }
     }
     
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector3(_xInput, 0, _zInput).normalized * speed;
-        
+        FlipX();
+    }
+
+    private void FlipX()
+    {
+        if (_xInput > 0)
+        {
+            _spriteRenderer.flipX = false;
+;        }
+        else if (_xInput < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
     }
 }
